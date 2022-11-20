dcc9ea7c6c52988391995cd6c95550b35766f7a3
Azul <avaca.azul@gmail.com>
2022-11-19 21:12:55 -0300

    clase 4 curso DOM

--- Javascript/DOM/script.js
+++ Javascript/DOM/script.js
@@ -1,20 +1,40 @@
+( () => {
 const btn = document.querySelector("[data-f-btn]");
-
+ 
 const createTask = (evento) => {
-    evento.preventDefault(); //para que no se refresque la pagina cada vez q sucede el evento
+    evento.preventDefault();
     const input = document.querySelector("[data-f-input]");
     const value = input.value;
     const list = document.querySelector("[data-list]")
     const task = document.createElement("li");
-    task.classList.add("card") //para que tenga el estilo de esa clase
+    task.classList.add("card") 
     input.value = "" //para que se borre el texto dsp de apretar el boton
-    const content = `<div>
-            <i class="far fa-check-square icon"></i>
-            <span class="task">${value}</span>
-        </div>
+    const taskContent = document.createElement("div");
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    const content = `
         <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content;
-    list.appendChild(task); //para que se creen "hijos" y se acumulen
-    console.log(content);
+    task.appendChild(taskContent);
+    list.appendChild(task); 
+
 } //guarda el contenido de input una vez que el boton es cliqueado y dsp lo muestra//
 btn.addEventListener ("click", createTask); 
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
+    return i;
+}
+
+const completeTask = (event) => {
+    console.log(event.target);
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+}
+})()
