d2baa15129c8396204eca78ecaf2da7864936209
Azul <avaca.azul@gmail.com>
2022-11-24 10:54:17 -0300

    curso: almacenando datos en el navegador

--- Javascript/DOM/script.js
+++ Javascript/DOM/script.js
@@ -1,25 +1,8 @@
-import checkComplete from "./components/checkComplete.js";
-import deleteIcon from "./components/deleteIcon.js";
+import { addTask } from "./components/addTask.js";
+import { displayTasks } from "./components/readTasks.js";
 const btn = document.querySelector("[data-f-btn]");
 
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-f-input]")
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card") ;
-    input.value = ""; //para que se borre el texto dsp de apretar el boton
-    const taskContent = document.createElement("div");
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    task.appendChild(taskContent);
-    task.appendChild(deleteIcon());
-    list.appendChild(task); 
-
-} //guaconst btn = document.querySelector("[data-f-btn]");
+ //guaconst btn = document.querySelector("[data-f-btn]");
     
-btn.addEventListener ("click", createTask); 
+btn.addEventListener ("click", addTask); 
+displayTasks();
